adb9e1e6e490850b3cbfd799efdd5cda816e18ff
Kevin St.Onge <kevinstonge@gmail.com>
2020-06-10 17:49:04 -0400

    completed first two eventListeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+//1. logo slideIn on load
+window.addEventListener('load',()=>{
+    document.querySelector("header>div>h1").classList.add("slideIn");
+})
+//2. rainbow bg on scroll
+let timer = null;
+window.addEventListener('scroll',()=>{
+    let body = document.querySelector("body");
+    if (timer !== null) {
+        body.classList.add("rainbowBG")
+        clearTimeout(timer);        
+    }
+    timer = setTimeout(function() {
+          body.classList.remove("rainbowBG");
+    }, 500);
+});
